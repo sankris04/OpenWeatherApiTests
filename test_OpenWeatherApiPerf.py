a8fc614fcc8018043a0b5ca42545c30c399dc141
sankris04 <sankris04@gmail.com>
2020-08-23 14:24:33 +0100

    Performance Test

---
+++ test_OpenWeatherApiPerf.py
@@ -0,0 +1,36 @@
+import requests
+import pytest
+import json
+import jsonpath
+import OpenAuth
+import requests_futures
+
+
+@pytest.fixture(scope='session')
+def apiauth():
+    apiauth = OpenAuth.ApiAuth()
+    yield apiauth
+
+
+def test_openWeatherAPIAuthResponseTime(apiauth):
+    # Get the base url
+
+    apiurl = apiauth.base_url()
+    # Get the appid
+    appid = apiauth.appid_key()
+    # Validate Response Time
+    response = requests.request('POST', apiurl + 'q=London&' + appid)
+    print(response.elapsed.total_seconds())
+    assert response.elapsed.total_seconds() <= 0.0250, "Response elpased time should be less than 250 ms"
+
+def test_openWeatherAPIAuthPrallelRequest(apiauth):
+    # Get the base url
+    apiurl = apiauth.base_url()
+    # Get the appid
+    appid = apiauth.appid_key()
+    # Validate Response Time
+
+    with requests.Session() as session:
+        session.get(apiurl + 'q=London&' + appid)
+
+
